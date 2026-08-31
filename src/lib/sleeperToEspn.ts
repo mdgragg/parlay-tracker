946c77eed94c6676d4050067ddbcdb5cdc184b07
Michael Gragg <mgragg@mjp.com>
2026-08-31 15:48:30 -0400

    updated mappings

--- src/lib/sleeperToEspn.ts
+++ src/lib/sleeperToEspn.ts
@@ -1,45 +1,46 @@
 export const sleeperToEspn: Record<string, { espnId: string; team: string }> = {
-  "": { espnId: "4430878", team: "SEA" }, // Burrow
+  "6770": { espnId: "3915511", team: "CIN" }, // Burrow
   "6797": { espnId: "4038941", team: "LAC" }, // Herbert
-  "": { espnId: "4430878", team: "SEA" }, // Daniels
+  "11566": { espnId: "4426348", team: "WAS" }, // Daniels
 
   "9226": { espnId: "4429160", team: "MIA" }, // Achane
-  "": { espnId: "4430878", team: "SEA" }, // Monty
+  "5892": { espnId: "4035538", team: "HOU" }, // Monty
   "7525": { espnId: "4241478", team: "PHI" }, // D. Smith
 
   "8151": { espnId: "4567048", team: "KC" }, // Walker
   "7526": { espnId: "4372016", team: "DEN" }, // Waddle
   "12512": { espnId: "4685702", team: "CLE" }, // Judkins
 
-  "": { espnId: "4567048", team: "LAC" }, // Hampton
+  "12507": { espnId: "4685382", team: "LAC" }, // Hampton
   "11620": { espnId: "4431299", team: "CHI" }, // Odunze
-  "": { espnId: "4685702", team: "CLE" }, // Flowers
+  "9997": { espnId: "4429615", team: "BAL" }, // Flowers
 
-  "": { espnId: "4567048", team: "KC" }, // Loveland
-  "": { espnId: "4431299", team: "CHI" }, // Jefferson
-  "": { espnId: "4685702", team: "CLE" }, // Evans
+  "12517": { espnId: "4723086", team: "CHI" }, // Loveland
+  "13524": { espnId: "4262921", team: "MIN" }, // Jefferson
+  "2216": { espnId: "16737", team: "SF" }, // Evans
+  "4217": { espnId: "3040151", team: "SF" }, // Kittle
 
-  "": { espnId: "4567048", team: "LAC" }, // Ladd
+  "11635": { espnId: "4612826", team: "LAC" }, // Ladd
 
   "9221": { espnId: "4429795", team: "DET" }, // Gibbs
   "9488": { espnId: "4430878", team: "SEA" }, // JSN
-  "": { espnId: "4430878", team: "SEA" }, // Nabers
+  "11632": { espnId: "4595348", team: "NYG" }, // Nabers
+  "11604": { espnId: "4432665", team: "LV" }, // Bowers
 
   "8150": { espnId: "4430737", team: "LAR" }, // Kyren
 
-  "": { espnId: "4432708", team: "ARI" }, // Terry
+  "5927": { espnId: "3121422", team: "WAS" }, // Terry
   "11628": { espnId: "4432708", team: "ARI" }, // Harrison jr
-  "": { espnId: "4569618", team: "NYJ" }, // Olsve
+  "8144": { espnId: "4361370", team: "NO" }, // Olave
   "8146": { espnId: "4569618", team: "NYJ" }, // Garrett Wilson
 
-  "": { espnId: "4432708", team: "ARI" }, // St. Brown
-  "": { espnId: "4432708", team: "ARI" }, // Chase Brown
-  "": { espnId: "4569618", team: "NYJ" }, // Bijan
-  "": { espnId: "4569618", team: "NYJ" }, // Drake
-  "": { espnId: "4569618", team: "NYJ" }, // Love
-  "": { espnId: "4569618", team: "NYJ" }, // McBride
-
-  "4217": { espnId: "3040151", team: "SF" }, // Kittle
+  "7547": { espnId: "4374302", team: "DET" }, // St. Brown
+  "7564": { espnId: "4362628", team: "CIN" }, // Chase
+  "9224": { espnId: "4362238", team: "CIN" }, // Chase Brown
+  "9509": { espnId: "4430807", team: "ATL" }, // Bijan
+  "8112": { espnId: "4426502", team: "ATL" }, // Drake
+  "13287": { espnId: "4870808", team: "ARI" }, // Love
+  "8130": { espnId: "4361307", team: "ARI" }, // McBride
 };
 
 // https://api.sleeper.app/v1/players/nfl
